d59b74feec4247f8b12b64845732adaa88ff8733
gianramirez <radicgian@gmail.com>
2022-06-04 10:11:45 -0400

    created reviews template. added @requestmapping to reviewcontroller

--- src/main/java/org/wecancoeit/reviews/controller/ReviewController.java
+++ src/main/java/org/wecancoeit/reviews/controller/ReviewController.java
@@ -1,6 +1,9 @@
 package org.wecancoeit.reviews.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.wecancoeit.reviews.repo.ReviewRepository;
 
 import javax.annotation.Resource;
@@ -10,6 +13,17 @@ public class ReviewController {
 
     @Resource
     private ReviewRepository reviewRepo;
+
+    @RequestMapping("/reviews")
+    public String findAllReviews(Model model){
+        model.addAttribute("coursesModel", reviewRepo.findAll());
+        return "reviewsTemplate";
+    }
+
+    @RequestMapping("/review")
+    public String findOneReview(@RequestParam(value = "id") Long id){
+        return "reviewTemplate";
+    }
 
 
 }
