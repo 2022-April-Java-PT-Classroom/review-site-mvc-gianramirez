0f356e085e9090c4aa6db87fa814935a38cafa4f
gianramirez <radicgian@gmail.com>
2022-06-06 21:45:38 -0400

    completed templates/testing. Getting 500 error for reviewsTemplate.

--- src/main/java/org/wecancoeit/reviews/repo/ReviewRepository.java
+++ src/main/java/org/wecancoeit/reviews/repo/ReviewRepository.java
@@ -1,7 +1,6 @@
 package org.wecancoeit.reviews.repo;
 
 
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.wecancoeit.reviews.model.Review;
 
@@ -13,6 +12,12 @@ import java.util.Map;
 public class ReviewRepository {
 
     Map<Long, Review> reviewsList = new HashMap<>();
+
+    public ReviewRepository(){
+        Review one = new Review(1L, " this", "this", "this", "this", "this");
+
+        reviewsList.put(one.getId(), one);
+    }
 
     public ReviewRepository(Review ... reviewsToAdd) {
         for(Review review: reviewsToAdd){
